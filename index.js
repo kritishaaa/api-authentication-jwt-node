9a5be8f21c04bd82bc2deb290696e9ae209c4df5
kritishaaa <kritishamoktan530@gmail.com>
2023-09-27 21:58:40 +0545

    user saved to db

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const connectDb= require('./database/connectDb')
 
 const app= express();
 
+app.use(express.json());
 //middleware
 app.use('/api/user', authRouter);
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,23 @@
 const router = require("express").Router();
+const User = require('../model/User')
 
-router.post('/register', (req, res) => {
-    res.send('Registration');
-})
-router.post('/login', (req, res) => {
-    res.send('Login')
+router.post('/register', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    try {
+        const saveUser = await user.save();
+        console.log(saveUser);
+        res.send(saveUser)
+    } catch (error) {
+        res.send(error);
+
+    }
 });
+
+
 
 module.exports = router;
