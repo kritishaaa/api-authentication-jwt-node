1e71d91e807a77efd6abca615965faa6c108f4db
kritishaaa <kritishamoktan530@gmail.com>
2023-09-28 22:36:40 +0545

    used joi validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,41 @@
 const router = require("express").Router();
-const User = require('../model/User')
+const User = require('../model/User');
+const Joi = require('@hapi/joi');
+
+//VALIDATION
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().min(8).required()
+});
+
+
 
 router.post('/register', async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    });
 
-    try {
-        const saveUser = await user.save();
-        console.log(saveUser);
-        res.send(saveUser)
-    } catch (error) {
-        res.send(error);
+    const { error } = schema.validate(req.body);
+    if (error)
+        return res.status(400).send(error.details[0].message);
 
+
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        try {
+            const saveUser = await user.save();
+            console.log(saveUser);
+            res.send(saveUser)
+        } catch (error) {
+            res.send(error);
+
+        }
     }
-});
+
+
+);
 
 
 
