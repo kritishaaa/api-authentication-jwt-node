5b7b7dde29385ff195ccfbaa2837269ae9b39381
kritishaaa <kritishamoktan530@gmail.com>
2023-09-28 23:02:18 +0545

    schema validation done

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,30 @@
 const router = require("express").Router();
 const User = require('../model/User');
-const Joi = require('@hapi/joi');
-
-//VALIDATION
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().required().email(),
-    password: Joi.string().min(8).required()
-});
-
-
+const {registerValidation, loginValidation}= require('../validation')
 
 router.post('/register', async (req, res) => {
 
-    const { error } = schema.validate(req.body);
+    
+    const {error} = registerValidation(req.body)
     if (error)
-        return res.status(400).send(error.details[0].message);
+        return res.status(400).send(error);
 
 
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        });
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
 
-        try {
-            const saveUser = await user.save();
-            console.log(saveUser);
-            res.send(saveUser)
-        } catch (error) {
-            res.send(error);
+    try {
+        const saveUser = await user.save();
+        console.log(saveUser);
+        res.send(saveUser)
+    } catch (error) {
+        res.send(error);
 
-        }
     }
+}
 
 
 );
---
+++ validation.js
@@ -0,0 +1,36 @@
+const Joi = require('@hapi/joi');
+
+//VALIDATION
+const registerValidation = (data) => {
+
+    const schema = Joi.object({
+        name: Joi.string()
+            .min(6)
+            .required(),
+        email: Joi.string()
+            .required()
+            .email(),
+        password: Joi.string()
+            .min(8)
+            .required(),
+
+    });
+    return schema.validate(data);
+};
+module.exports.registerValidation = registerValidation;
+
+const loginValidation = (data) => {
+
+    const schema = Joi.object({
+       
+        email: Joi.string()
+            .required()
+            .email(),
+        password: Joi.string()
+            .min(8)
+            .required(),
+
+    });
+    return schema.validate(data);
+};
+module.exports.loginValidation = loginValidation;
